f89eb5483ec2cb596ef2585c4759af03820cdca9
naibor <liznaibor.en@gmail.com>
2018-05-23 17:56:45 +0300

    [#157736626][ft]adds modal to navigation bar

--- UI/static/JS/myjs.js
+++ UI/static/JS/myjs.js
@@ -1,6 +1,27 @@
-// document ready function to prevent jQuery code from running before document is finished loading
-$(document).ready(function(){
-    $("button").click(function(){
-        $(".navbar-item").css({"display":"block"})
-    });
-});
+// About modal script
+// get modal
+var modal = document.getElementById("AbtModal");
+
+// get button that opens modal
+var btn = document.getElementById("Abtlink");
+
+//get span element that closes moadal
+var span = document.getElementsByClassName("close")[0];
+
+
+// open on click
+btn.onclick = function (){
+    modal.style.display = "block";
+}
+
+//  close when span element is clicked
+span.onclick = function() {
+    modal.style.display ="none";
+
+}
+// closes modal when user clicks anywhere ouside modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none"
+    }
+}
