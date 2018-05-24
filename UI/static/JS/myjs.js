83bf1043ac398de2c6c823383581c12bc3eacb7b
naibor <liznaibor.en@gmail.com>
2018-05-24 14:07:56 +0300

    [finishes #157736626]Added modals to navigation bar items

--- UI/static/JS/myjs.js
+++ UI/static/JS/myjs.js
@@ -1,27 +1,43 @@
-// About modal script
-// get modal
-var modal = document.getElementById("AbtModal");
+// modal script
 
 // get button that opens modal
-var btn = document.getElementById("Abtlink");
+var btnA = document.getElementById("Abtlink");
 
-//get span element that closes moadal
-var span = document.getElementsByClassName("close")[0];
+var btnS = document.getElementById("Slink");
+
+// get  services modal
+var modalS = document.getElementById("SModal")
+
+// get about modal
+var modalAbt = document.getElementById("AbtModal");
+
+//get span element that closes About moadal
+var span = document.getElementsByClassName("close");
+
+// get the span element that closes the services modal
+// var Sspan = document.getElementsByClassName("close")[1];
 
 
-// open on click
-btn.onclick = function (){
-    modal.style.display = "block";
+
+// About modal functions
+
+// open About modal on click
+btnA.onclick = function(){
+    modalAbt.style.display="block";
+}
+//  close the about modal when span element is clicked
+span[0].onclick = function() {
+    modalAbt.style.display ="none";
+
 }
 
-//  close when span element is clicked
-span.onclick = function() {
-    modal.style.display ="none";
 
+// open service modal functions
+btnS.onclick = function () {
+    modalS.style.display = "block";
 }
-// closes modal when user clicks anywhere ouside modal
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none"
-    }
+//  close the about modal when span element is clicked
+span[1].onclick = function () {
+    modalS.style.display = "none";
+
 }
