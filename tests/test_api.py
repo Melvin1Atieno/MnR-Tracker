e251c34549bf85595ac53568c8d119015ab2988d
naibor <liznaibor.en@gmail.com>
2018-05-30 19:01:59 +0300

    [Feature #157931162] Adds test_case for put method

--- tests/test_api.py
+++ tests/test_api.py
@@ -19,13 +19,11 @@ class RequestsApiTestcase(unittest.TestCase):
         
         self.client = self.app.test_client()
 
-        self.request = {
-            "request_title":"laptop", "request_description":"black screen","request_category":"Repair"
-        }
     def test_request_creation(self):
         """tests API can create a request"""
         create = self.client.post("/api/v1/requests", data= json.dumps(dict(request_title="laptop",request_description="whiteScreen",
         request_category="repair")), content_type ="application/json")
+        print(create)
         response = json.loads(create.data.decode())
         self.assertIn("Request successfully created", response["message"])
 
@@ -35,21 +33,29 @@ class RequestsApiTestcase(unittest.TestCase):
         create = self.client.post("/api/v1/requests", data=json.dumps(dict(request_title="laptop",request_description="whiteScreen",
          request_category="repair")),content_type ="application/json")
         results = self.client.get("/api/v1/requests/")
-        response = json.loads(results.data.decode("utf-8"))
+        response = json.loads(results.data.decode())
         created_request = response[0]
         self.assertEqual(created_request["request_title"],"laptop")
 
     def test_get_method_returns_request_by_id(self):
-        create = self.client.post("/api/v1/request", date=json.dumps(dict(request_title="laptop",request_description="whiteScreen",
-        request_category="repair")))
+        create = self.client.post("/api/v1/request", data=json.dumps(dict(request_title="laptop",request_description="whiteScreen",
+        request_category="repair")), content_type="application/json")
         myrequest = self.client.get("/api/v1/request/0")
-        response = json.loads(myrequest.data.decode("utf-8"))
+        response = json.loads(myrequest.data.decode())
         self.assertIn("whitescreen", response["request_description"])
-        
-
-
-
 
     
-if __name__ == "__main__":
-    unittest.main()
+    def test_requests_can_be_edited(self):
+        """test requets can be edited"""
+        response = self.client.post("/api/v1/request/",
+        data=json.dumps(dict(request_title="windows", request_description="dirty", request_category="maintenance")),content_type="application/json")
+        change = self.client.put("/api/v1/request/0", data=json.dumps(dict(request_title="kitchen", 
+        request_description="dirty",request_category="maintenance")), content_type="application/json")
+        results = self.client.get("/api/v1/request/0")
+        self.assertIn(results["request_title"], "kitchen")
+        
+        
+        
+        
+        if __name__ == "__main__":
+            unittest.main()
